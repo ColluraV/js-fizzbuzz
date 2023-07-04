278fd8a5902c6cccc63d5fa9ed74703db2547f54
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-04 16:48:31 +0200

    terminato

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,39 @@
 const blocchiContainer = document.querySelector(".blocchi-container")
 
 for ( let i = 1 ; i <= 100; i++){
-    
+
+    /*`<div class="bloccoNumero"></div>`*/
+    let singoloBlocco =  document.createElement("div");
+    singoloBlocco.classList.add("bloccoNumero")
+    blocchiContainer.append(singoloBlocco);
+
     if ((i %3 == 0) && (i %5 == 0)){
         console.log('FizzBuzz');
+        singoloBlocco.innerHTML = ('Fizz Buzz');
+        singoloBlocco.classList.add("bg-warning");
     }
 
      else if (i %3 ==  0 ){
         console.log('Fizz');
+        singoloBlocco.innerHTML = ('Fizz');
+        singoloBlocco.classList.add("bg-success");
+
     }
 
     else if ( i %5 == 0) {
         console.log('Buzz');
+        singoloBlocco.innerHTML = ('Buzz');
+        singoloBlocco.classList.add("bg-danger");
+
     }
 
     else {
         console.log(i);
+        singoloBlocco.innerHTML = (i);
+
     }
+
+
 }
 
 
